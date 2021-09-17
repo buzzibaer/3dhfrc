bc75d87fe702445923f7e51113b06e0912ee6cf8
buzzibaer <buzzibaer@bust.name>
2021-09-17 14:54:16 +0200

    major refactoring

---
+++ src/threedpinballfan/Fan.java
@@ -0,0 +1,42 @@
+package threedpinballfan;
+
+import java.util.logging.Logger;
+
+public class Fan {
+
+	private static Logger logger;
+	private static TcpClient client;
+
+	public Fan(Logger logger, TcpClient client) {
+		Fan.logger = logger;
+		Fan.client = client;
+	}
+
+	/**
+	 * Setting 3d wifi fan to single vid play mode the fan usually rotates / cylces
+	 * to all videos on the SD card, we wannt to have it only in playback mode for a
+	 * singe file
+	 * 
+	 * @param client
+	 */
+	public void selectSingePlaybackMode() {
+		String playModelSingle = UpLoadData.playModelSingle();
+		client.send(playModelSingle);
+		logger.info("Command Send = playModelSingle > " + playModelSingle);
+	}
+
+	/**
+	 * Selection of specific table on the wifi fan
+	 * 
+	 * @param args
+	 * @param client
+	 */
+	public static void selectVidForTable(int videoid) {
+		String playFile = UpLoadData.playFile(videoid);
+
+		client.send(playFile);
+		logger.info("DarkPrincess detected!");
+		logger.info("Command Send = playFile > " + playFile);
+	}
+
+}
--- src/threedpinballfan/Lauch.java
+++ src/threedpinballfan/Lauch.java
@@ -1,62 +1,94 @@
 package threedpinballfan;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.sun.media.jfxmedia.events.NewFrameEvent;
+
+import picocli.CommandLine;
 
 /**
  * 
- * Lauch class for selection of a specific video / picture on a 3d holografic fan
+ * Lauch class for selection of a specific video / picture on a 3d holografic
+ * fan
+ * 
  * @author buzzibaer
  *
  */
 public class Lauch {
-	
+
 	static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	static TcpClient client;
+	static Fan fan;
 
 	/**
 	 * CommandLine starting method
-	 * @param args expects the tablename from commandline from pinupopper or other system for choosing the right video / pic
+	 * 
+	 * @param args expects the tablename from commandline from pinupopper or other
+	 *             system for choosing the right video / pic
 	 * @throws InterruptedException
 	 * @throws SecurityException
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws InterruptedException, SecurityException, IOException {
 
+//		int exitCode = new CommandLine(new fancommands()).execute(args);
+//		System.exit(exitCode);
+
 		initLogger();
-		
+
 		// Printing CommandLine Parameters
 		logger.info("Startparameter =  " + args[0]);
 
+		// Reading config file
+		java.util.Properties props = readPropertiesFile();
+		logger.info("Properties = " + props);
+		int videoid = Integer.parseInt( props.getProperty(args[0], "1"));
+		logger.info("selcted video id from mapping = " + props);
 
-		
+		// init TCP connection
+		client = initTcpClient();
+		fan = new Fan(logger,client);
 
+		// single playback mode selection
+		fan.selectSingePlaybackMode();
 
-		TcpClient client = initTcpClient();
-
-
-		fanSelectSingePlaybackMode(client);
-
-
-		fanSelectVidForTable(args, client);
-
-
-
+		// select pic / video
+		fan.selectVidForTable(videoid);
 
 		logger.info("Exit");
 		System.exit(0);
 	}
 
 	/**
+	 * 
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	private static java.util.Properties readPropertiesFile() throws FileNotFoundException, IOException {
+		java.util.Properties props = new java.util.Properties();
+		java.io.FileInputStream fis = new java.io.FileInputStream(new java.io.File("tabletovideomapping.ini"));
+		props.load(fis);
+		fis.close();
+		return props;
+	}
+
+	/**
 	 * Logger and level initiation
+	 * 
 	 * @throws IOException
 	 */
 	private static void initLogger() throws IOException {
-		
+
 		logger.setLevel(Level.ALL);
 		Handler handler = new ConsoleHandler();
 		handler.setLevel(Level.ALL);
@@ -69,36 +101,14 @@ public class Lauch {
 
 		logger.info("Starte");
 
-		
 	}
 
-	/**
-	 * Selection of specific table on the wifi fan
-	 * @param args
-	 * @param client
-	 */
-	private static void fanSelectVidForTable(String[] args, TcpClient client) {
-		String playFile4 = UpLoadData.playFile(4);
-		if (args[0].equalsIgnoreCase("Darkprincess")) {
-			client.send(playFile4);
-			logger.info("DarkPrincess detected!");
-			logger.info("Command Send = playFile > " + playFile4);
-		}
-	}
 
-	/**
-	 * Setting 3d wifi fan to single vid play mode
-	 * the fan usually rotates / cylces to all videos on the SD card, we wannt to have it only in playback mode for a singe file
-	 * @param client
-	 */
-	private static void fanSelectSingePlaybackMode(TcpClient client) {
-		String playModelSingle = UpLoadData.playModelSingle();
-		client.send(playModelSingle);
-		logger.info("Command Send = playModelSingle > " + playModelSingle);
-	}
+
 
 	/**
 	 * Opening a TCP channel to the WiFi Module on the 3D Fan
+	 * 
 	 * @return
 	 */
 	private static TcpClient initTcpClient() {
@@ -107,5 +117,20 @@ public class Lauch {
 		client.connSocket();
 		logger.info("Socket open");
 		return client;
+	}
+
+	/**
+	 * Load a Properties File
+	 * 
+	 * @param propsFile
+	 * @return Properties
+	 * @throws IOException
+	 */
+	public static Properties load(File propsFile) throws IOException {
+		Properties props = new Properties();
+		FileInputStream fis = new FileInputStream(propsFile);
+		props.load(fis);
+		fis.close();
+		return props;
 	}
 }
