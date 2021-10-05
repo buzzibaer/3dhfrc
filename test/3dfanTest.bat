67c99abbadb263854c40fc10e4ebbe5e34ef3989
buzzibaer <buzzibaer@bust.name>
2021-10-05 22:02:10 +0200

    Test BAT File

---
+++ test/3dfanTest.bat
@@ -0,0 +1,6 @@
+rem @echo off
+rem START /min "" /i /d "C:\tdhfrc\" java -jar tdhfrc.jar Tales from the Crypt (Data East 1993)_VPW_V1.01
+rem START /min "" /i /d "C:\tdhfrc\" java -jar tdhfrc.jar Space Shuttle (Williams 1984)
+START /min "" /i /d "C:\tdhfrc\" java -jar tdhfrc.jar Super Mario Bros. (Premier 1992)
+pause
+exit
